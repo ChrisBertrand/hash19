c906a298737354c52df14b43e695cde8078a2b4a
chrisbertrandsagecom <chris.bertrand@sage.com>
2019-02-28 12:31:54 +0000

    Added Typescript entry

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-
-function readFile(filename){
-    const fs = require("fs");
-    const inputFile = filename;
-    
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+function readFile(filename) {
+    var inputFile = filename;
     // Read data from file
     var data = fs.readFileSync(inputFile, "utf-8");
-    
     var dataByLine = data.split("\n");
-    
     // Establish parameters
     var firstRow = dataByLine[0].split(" ");
 }
-
 function createOutput(obj) {
     //console.log(slices);
     var wstream = fs.createWriteStream('file.output');
     wstream.write(obj.length + '\n');
-    obj.forEach(s => wstream.write(`${s} \n`));
+    obj.forEach(function (s) { return wstream.write(s + " \n"); });
     wstream.end();
 }
-
 readFile("./input/a_example.in");
---
+++ index.ts
@@ -0,0 +1,24 @@
+
+import * as fs from 'fs';
+
+function readFile(filename: string) {
+    const inputFile = filename;
+    
+    // Read data from file
+    let data = fs.readFileSync(inputFile, "utf-8");
+    
+    let dataByLine = data.split("\n");
+    
+    // Establish parameters
+    let firstRow = dataByLine[0].split(" ");
+}
+
+function createOutput(obj : any) {
+    //console.log(slices);
+    let wstream = fs.createWriteStream('file.output');
+    wstream.write(obj.length + '\n');
+    obj.forEach(s => wstream.write(`${s} \n`));
+    wstream.end();
+}
+
+readFile("./input/a_example.in");
