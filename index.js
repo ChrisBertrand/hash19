b5a834d1a93bdf52f4eac9b50476d98654a8ab74
chrisbertrandsagecom <chris.bertrand@sage.com>
2019-02-28 18:53:17 +0000

    Working

--- index.js
+++ index.js
@@ -1,19 +1,126 @@
 "use strict";
 exports.__esModule = true;
 var fs = require("fs");
+var files = [
+    "./files/a_example.txt",
+    "./files/b_lovely_landscapes.txt",
+    "./files/c_memorable_moments.txt",
+    "./files/d_pet_pictures.txt",
+    "./files/e_shiny_selfies.txt"
+];
+var numPhotos;
+var photos = [];
+var horizontalPhotos = [];
+var verticalPhotos = [];
+var allTags = new Map();
 function readFile(filename) {
     var inputFile = filename;
     // Read data from file
     var data = fs.readFileSync(inputFile, "utf-8");
     var dataByLine = data.split("\n");
+    var firstLine = dataByLine.shift();
+    numPhotos = parseFloat(firstLine);
+    var photoIndex = 0;
+    var _loop_1 = function (line) {
+        if (!line) {
+            return "continue";
+        }
+        var parts = line.split(" ");
+        var orientation_1 = parts[0];
+        var numTags = parseFloat(parts[1]);
+        var tags = [];
+        for (var i = 0; i < numTags; i++) {
+            tags = tags.concat([parts[i + 2]]);
+        }
+        var photo = {
+            id: photoIndex,
+            orientation: orientation_1,
+            tags: tags,
+            isSelected: false
+        };
+        photo.tags.forEach(function (tag) {
+            var photoIds = allTags.get(tag);
+            if (photoIds) {
+                photoIds.push(photo.id);
+            }
+            else {
+                photoIds = [photo.id];
+            }
+        });
+        photoIndex++;
+        photos.push(photo);
+    };
     // Establish parameters
-    var firstRow = dataByLine[0].split(" ");
+    for (var _i = 0, dataByLine_1 = dataByLine; _i < dataByLine_1.length; _i++) {
+        var line = dataByLine_1[_i];
+        _loop_1(line);
+    }
+    horizontalPhotos = photos.filter(function (photo) { return photo.orientation === 'H'; });
+    verticalPhotos = photos.filter(function (photo) { return photo.orientation === 'V'; });
 }
-function createOutput(obj) {
-    //console.log(slices);
-    var wstream = fs.createWriteStream('file.output');
-    wstream.write(obj.length + '\n');
-    obj.forEach(function (s) { return wstream.write(s + " \n"); });
+function createOutput(slideshow, file) {
+    console.log('file', file);
+    var wstream = fs.createWriteStream(".output/" + file + ".output");
+    wstream.write(slideshow.slides.length + '\n');
+    slideshow.slides.forEach(function (s) {
+        s.photos.forEach(function (photo) {
+            wstream.write(photo.id + " ");
+        });
+        wstream.write("\n");
+    });
     wstream.end();
 }
-readFile("./input/a_example.in");
+function getScore(slideshow) {
+    //slideshow.slides.forEach(slide => slide..)
+}
+function compareSlides(slide1, slide2) {
+    var factor1;
+    var factor2;
+    var factor3;
+}
+function createVerticalSlide(photo1, photo2) {
+    var tags = [];
+    tags = tags.concat(photo1.tags);
+    tags = tags.concat(photo2.tags);
+    var tagSet = new Set(tags);
+    var slide = {
+        photos: photos,
+        tags: Array.from(tagSet.values())
+    };
+    return slide;
+}
+function createHorizontalSlide(photo) {
+    return {
+        photos: [photo],
+        tags: photo.tags
+    };
+}
+function solve() {
+    var slides = [];
+    for (var _i = 0, horizontalPhotos_1 = horizontalPhotos; _i < horizontalPhotos_1.length; _i++) {
+        var photo = horizontalPhotos_1[_i];
+        var slide = createHorizontalSlide(photo);
+        slides.push(slide);
+    }
+    for (var i = 0; i < verticalPhotos.length; i += 2) {
+        if (i + 1 >= verticalPhotos.length) {
+            // We have run out of photos to pair with!
+            break;
+        }
+        var slide = createVerticalSlide(verticalPhotos[i], verticalPhotos[i + 1]);
+        slides.push(slide);
+    }
+    return slides;
+}
+// Solve all files
+files.forEach(function (f) {
+    // Solve this file
+    readFile(f);
+    var slides = solve();
+    // Create output
+    var slideshow = {
+        slides: slides,
+        score: 0
+    };
+    createOutput(slideshow, f.substring(f.lastIndexOf('/') + 1, f.lastIndexOf(".")));
+});
--- index.ts
+++ index.ts
@@ -1,25 +1,172 @@
 
 import * as fs from 'fs';
 
+const files = [
+    "./files/a_example.txt",
+    "./files/b_lovely_landscapes.txt",
+    "./files/c_memorable_moments.txt",
+    "./files/d_pet_pictures.txt",
+    "./files/e_shiny_selfies.txt"
+];
+
+let numPhotos: number;
+let photos: Photo[] = [];
+
+let horizontalPhotos: Photo[] = [];
+let verticalPhotos: Photo[] = [];
+
+let allTags: Map<string, number[]> = new Map<string, number[]>();
+
+interface Photo {
+    id: number;
+    orientation: string;
+    tags: string[];
+    isSelected: boolean;
+}
+
+interface Slide {
+    photos: Photo[];
+    tags: string[];
+}
+
+interface Slideshow {
+    slides: Slide[],
+    score: number
+}
+
 function readFile(filename: string) {
     const inputFile = filename;
     
     // Read data from file
     let data = fs.readFileSync(inputFile, "utf-8");
     
-    let dataByLine = data.split("\n");
-    
+    let dataByLine: string[] = data.split("\n");
+
+    const firstLine: string = <string> dataByLine.shift();
+    numPhotos = parseFloat(firstLine);
+
+    let photoIndex: number = 0;
+
     // Establish parameters
-    let firstRow = dataByLine[0].split(" ");
+    for (const line of dataByLine) {
+
+        if (!line) {
+            continue;
+        }
+
+        let parts: string[] = line.split(" ");
+        let orientation: string = parts[0];
+        let numTags: number = parseFloat(parts[1]);
+        let tags: string[] = [];
+        
+        for (let i = 0; i < numTags; i++) {
+            tags = [...tags, parts[i+2]];
+        }
+
+        let photo: Photo = {
+            id: photoIndex,
+            orientation,
+            tags,
+            isSelected: false
+        };
+
+        photo.tags.forEach(tag => {
+            let photoIds: number[] | undefined = allTags.get(tag);
+            if (photoIds) {
+                photoIds.push(photo.id);
+            } else {
+                photoIds = [photo.id];
+            }
+        });
+
+        photoIndex++;
+        photos.push(photo);
+    }
+    
+    horizontalPhotos = photos.filter(photo => photo.orientation === 'H');
+    verticalPhotos = photos.filter(photo => photo.orientation === 'V');
 }
 
-function createOutput(obj : any) {
-    //console.log(slices);
-    let wstream = fs.createWriteStream('file.output');
-    wstream.write(obj.length + '\n');
-    obj.forEach(s => wstream.write(`${s} \n`));
+function createOutput(slideshow : Slideshow, file: string) {
+    console.log('file', file);
+    let wstream = fs.createWriteStream(`.output/${file}.output`);
+    wstream.write(slideshow.slides.length + '\n');
+    slideshow.slides.forEach((s: Slide) => {
+        s.photos.forEach((photo) => {
+            wstream.write(`${photo.id} `)
+        });
+        wstream.write(`\n`);
+    });
     wstream.end();
 }
+ 
+function getScore(slideshow: Slideshow) {
+ //slideshow.slides.forEach(slide => slide..)
+}
 
-readFile("./input/a_example.in");
+function compareSlides(slide1: Slide, slide2: Slide) {
+    let factor1: number; 
+    let factor2: number;
+    let factor3: number;
+}
 
+function createVerticalSlide(photo1: Photo, photo2: Photo) {
+    let tags: string[] = [];
+    tags = tags.concat(photo1.tags);
+    tags = tags.concat(photo2.tags);
+    let tagSet: Set<string> = new Set(tags);
+    let slide: Slide = {
+        photos,
+        tags: Array.from(tagSet.values())
+    };
+    return slide;
+}
+
+function createHorizontalSlide(photo: Photo) {
+    return {
+        photos: [photo],
+        tags: photo.tags
+    };
+}
+
+function solve() {
+
+    const slides: Slide[] = [];
+
+    for (const photo of horizontalPhotos) {
+        let slide: Slide = createHorizontalSlide(photo);
+        slides.push(slide);
+    }
+
+    for (let i = 0; i < verticalPhotos.length; i += 2) {
+        if (i + 1 >= verticalPhotos.length) {
+            // We have run out of photos to pair with!
+            break;
+        }
+        let slide: Slide = createVerticalSlide(
+            verticalPhotos[i],
+            verticalPhotos[i + 1]
+        );
+        slides.push(slide);
+    }
+
+    return slides;
+}
+
+// Solve all files
+files.forEach(f => {
+    
+    // Solve this file
+    readFile(f);
+    const slides: Slide[] = solve();
+
+    // Create output
+    const slideshow: Slideshow = {
+        slides,
+        score: 0
+    };
+    createOutput(
+        slideshow, 
+        f.substring(f.lastIndexOf('/')+1, f.lastIndexOf("."))
+    );
+});
